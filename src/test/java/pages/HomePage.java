d01bb51619eb1a230da7cce6f05ec92c356d66e9
Robert Rice <robertrice12009@yahoo.com>
2022-07-02 16:01:10 -0500

    firstrobert

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,26 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+//
+public class HomePage {
+
+
+    public HomePage() {
+       PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//li[@class='dropdown nav-item']")
+    public WebElement loginDropDown;
+    @FindBy(id =  "login-item")
+    public WebElement loginSignInButton;
+    @FindBy(id = "username")
+    public WebElement username;
+    @FindBy(id = "password")
+    public WebElement password;
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement signInbutton;
+
+}

8cd5c8b752e93bd69a5c463f24b1b2eb0a7e197c
Enes Karaaslan <fromsmyrna237@gmail.com>
2022-07-03 10:14:00 -0400

    hook

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -12,8 +12,10 @@ import org.junit.runner.RunWith;
 
         },
         features = "./src/test/resources/features",
-        glue = {"stepdefinitions","Hooks"},
+        glue = {"stepdefinitions"},
         tags = "  @InvalidpasswordScenario",
+
+
         dryRun = false
 )
 public class Runner {

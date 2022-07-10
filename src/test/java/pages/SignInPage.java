ac3c789895a79de3a56fbd9cf970db0df566d15d
“Yelbaygit config --global user.email cnntylby@gmail.comgit config --global credential.username Yelbaygit config --global credential.password Cennet1983 <cnntylby@gmail.comgit config --global user.name Yelbay>
2022-07-09 21:02:27 -0400

    new update

--- src/test/java/pages/SignInPage.java
+++ src/test/java/pages/SignInPage.java
@@ -1,24 +1,23 @@
 package pages;
 
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import utilities.Driver;
+        import org.openqa.selenium.support.FindBy;
+        import org.openqa.selenium.support.PageFactory;
+        import utilities.Driver;
 
 public class SignInPage {
-    public SignInPage(){
+    public SignInPage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
-    @FindBy(id="account-menu")
+
+    @FindBy(id = "account-menu")
     public WebElement accountMenu;
     @FindBy(linkText = "Sign in")
     public WebElement signInMenuButton;
-    @FindBy(id="username")
+    @FindBy(id = "username")
     public WebElement usernameTextBox;
-    @FindBy(id="password")
+    @FindBy(id = "password")
     public WebElement passwordTextBox;
-    @FindBy(xpath="//button[@type='submit']")
+    @FindBy(xpath = "//button[@type='submit']")
     public WebElement signInSubmitButton;
-
-
 }

15670fb266aa6484b833d4dda7b4fd38f52146ac
“Yelbaygit config --global user.email cnntylby@gmail.comgit config --global credential.username Yelbaygit config --global credential.password Cennet1983 <cnntylby@gmail.comgit config --global user.name Yelbay>
2022-07-11 19:05:57 -0400

    cennet update

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = {"stepdefinitions","Hooks"},
-        tags = "@US6Pos",
+        tags = "@US14DoctorSeeAndUpdateInpatientInfo",
         dryRun = false
 )
 public class Runner {

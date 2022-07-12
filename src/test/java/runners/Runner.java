52a6bbd4da6043711f76228f2047498555a78ef2
Cselcuk89 <cselcuk8991@gmail.com>
2022-07-12 16:48:53 -0500

    commit

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = {"stepdefinitions","Hooks"},
-        tags = "@AppointmentsAreVisible",
+        //tags = "@AppointmentsAreVisible",
         dryRun = false
 )
 public class Runner {

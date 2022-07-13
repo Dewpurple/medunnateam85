92d36996aa468e1d61b4458eddca0dd55188f509
VG <vusal_gasimov@yahoo.com>
2022-07-13 19:18:56 -0400

    vusal updates

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = {"stepdefinitions","Hooks"},
-        tags = "@TC_016_Not_Selecting_State",
+        tags = "@US01_TC01_positive",
         dryRun = false
 )
 public class Runner {

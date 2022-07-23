2e490ae3ab584d5a6a506615cb715cd13fefac71
Robert Rice <robertrice12009@yahoo.com>
2022-07-23 14:27:15 -0500

    dbsession

--- src/test/java/stepdefinitions/dbsteps/US_009_PatientInformationStepDefs.java
+++ src/test/java/stepdefinitions/dbsteps/US_009_PatientInformationStepDefs.java
@@ -1,8 +1,11 @@
 package stepdefinitions.dbsteps;
 
 import io.cucumber.java.en.Given;
+import org.junit.Assert;
 import utilities.DBUtils;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class US_009_PatientInformationStepDefs {
@@ -11,16 +14,56 @@ public class US_009_PatientInformationStepDefs {
     public void make_connection_with_db_us009() {
         DBUtils.createConnection();
     }
-    @Given("user gets the {string} from {string} table")
-    public void user_gets_the_from_table(String column, String table) {
-    String myDynamicQuery = "Select " + column + " from " + table;
-    DBUtils.executeQuery(myDynamicQuery);
-    List <Object> allColumnData = DBUtils.getColumnData("Select " + column + " from " + table, "id");
-        System.out.println(allColumnData);
-    }
-    @Given("user gets the data")
-    public void user_gets_the_data() {
 
+    @Given("user gets the {string} from {string} table where {string} equals {string}")
+    public void user_gets_the_from_table_where_equals(String column, String table, String key, String id) throws InterruptedException {
+    String myDynamicQuery = "Select " +column + " from " + table + " where " + key+"="+id;
+    DBUtils.executeQuery(myDynamicQuery);
+        List <Object> allColumnData = DBUtils.getRowList("Select " +column + " from " + table + " where " + key+"="+id);
+        System.out.println(allColumnData);
+        List <Object> expectedData = new ArrayList<>();
+        expectedData.add(id);
+        expectedData.add("Vusal");
+        System.out.println(expectedData);
+        Thread.sleep(500);
+        Assert.assertTrue(allColumnData.containsAll(expectedData));
+
+    }
+
+    @Given("user enters expected data and performs assertion")
+    public void user_enters_expected_data_and_performs_assertion() {
+
+    }
+//    @Given("user gets the {string} from {string} table")
+//    public void user_gets_the_from_table(String column, String table) {
+//    String myDynamicQuery = "Select " + column + " from " + table;
+//    DBUtils.executeQuery(myDynamicQuery);
+//    }
+    @Given("user reads all of the {string} column data US009")
+    public void user_reads_all_of_the_column_data_us009(String column) throws SQLException {
+//        DBUtils.getResultset().next();
+//        Object columndata = DBUtils.getResultset().getObject(column);
+//        System.out.println(columndata);
+//        DBUtils.getResultset().next();
+//        Object columndata2 = DBUtils.getResultset().getObject(column);
+//        DBUtils.getResultset().next();
+//        Object columndata3 = DBUtils.getResultset().getObject(column);
+
+//        while (DBUtils.getResultset().next()){
+//            Object eachColumnData = DBUtils.getResultset().getObject(column);
+//            System.out.println(eachColumnData);
+//        }
+//        List <Object> allColumnData = DBUtils.getColumnData("Select * from " + table, "id");
+//        System.out.println(allColumnData);
+
+    }
+    @Given("verify {string} table  {string} column contains the {string}")
+    public void verify_table_column_contains_the(String table, String column, String data) {
+//   List <Object> allColumnData = DBUtils.getColumnData("Select * from  jhi_user where", "id");
+//        System.out.println(allColumnData);
+//        List <Object> expectedData = new ArrayList<>();
+//        expectedData.add(data);
+//        Assert.assertTrue(allColumnData.containsAll(expectedData));
     }
     @Given("verify {string} table contains the expected data")
     public void verify_table_contains_the_expected_data(String string) {
--- src/test/resources/features/dbfeatures/US_009_DP_PatientDetails.feature
+++ src/test/resources/features/dbfeatures/US_009_DP_PatientDetails.feature
@@ -2,7 +2,9 @@ Feature: US_009_DP_Patient_Details
   @Api
   Scenario: US_009patientdetails
     Given Make connection with DB US009
-    And user gets the "*" from "jhi_user" table
-    # select * from jhi_user
-    And user gets the data
-    And verify "jhi_user" table contains the expected data
+    And user gets the "*" from "jhi_user" table where "id" equals "99802"
+    And user enters expected data and performs assertion
+
+#    And user reads all of the "id" column data US009
+#    And verify "jhi_user" table  "id" column contains the "2224"
+    Then close the database connection 007

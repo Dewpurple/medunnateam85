df951983c0789ec6bb663fd6e6b880ae5bf58142
Shebnem Bayezit <sebnembayezit2022@gmail.com>
2022-07-29 23:41:31 -0400

    u

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "./src/test/resources/features",
         glue = {"stepdefinitions","Hooks"},
 
-        tags = "@smoke",
+        tags = "@Api_Get_SSN_API",
 
         dryRun = false
 )
--- src/test/resources/features/apifeatures/US_001_SSN.feature
+++ src/test/resources/features/apifeatures/US_001_SSN.feature
@@ -1,6 +1,6 @@
 Feature: US001_SSN number is check in API test
-
-  @Api_Get
+  @regression
+  @Api_Get_SSN_API
   @Api
     Scenario: TC01_Get_Data
     Given US_01 user set the url and get data and verify it with generate token
